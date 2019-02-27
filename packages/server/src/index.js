c6be69c1dfb710ec5e71b70377bb7a11c4ec6973
Yongfeng LI <wliyongfeng@gmail.com>
2019-02-27 15:36:32 +0800

    Introduce dotenv to server project

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -4,6 +4,7 @@ const logger = require("koa-logger");
 const helmet = require("koa-helmet");
 const http = require("http");
 const db = require(__dirname + "/../models");
+require("dotenv").config();
 
 const app = new Koa();
 
@@ -17,5 +18,9 @@ require("./routes")(app);
 const server = http.createServer(app.callback());
 db.sequelize.authenticate().then(() => {
   app.context.db = db;
-  server.listen(3000);
+
+  const port = process.env.SERVER_PORT || 3001;
+  server.listen(port, () =>
+    console.log(`✅  The server is running at http://localhost:${port}/`)
+  );
 });
