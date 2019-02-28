ef6356fd585b3e56f7530b918161c02361281db9
Yongfeng LI <wliyongfeng@gmail.com>
2019-02-28 10:56:53 +0800

    Improve layout css

--- packages/site/src/App/Main/index.js
+++ packages/site/src/App/Main/index.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import { Redirect, Route, Switch } from "react-router";
 import Home from "./Home";
 import Block from "./Block";
+import "./index.scss";
 
 class Main extends Component {
   render() {
     return (
       <div className="main">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/block" component={Block} />
-          <Redirect to="/" />
-        </Switch>
+        <section className="body">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/block" component={Block} />
+            <Redirect to="/" />
+          </Switch>
+        </section>
       </div>
     );
   }
