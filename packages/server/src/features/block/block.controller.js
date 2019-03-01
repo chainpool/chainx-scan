a95ac09a3b61a27f48cd7abaffcb47df36664e95
Yongfeng LI <wliyongfeng@gmail.com>
2019-03-01 10:42:41 +0800

    Add getBlock api

--- packages/server/src/features/block/block.controller.js
+++ packages/server/src/features/block/block.controller.js
@@ -1,5 +1,21 @@
 const { extractPage, trimFields } = require("../utils");
 
+function normalizeBlock(block) {
+  const fieldsNeedTrim = [
+    "hash",
+    "parent_hash",
+    "state_root",
+    "extrinsics_root"
+  ];
+
+  return {
+    ...block,
+    ...trimFields(block, fieldsNeedTrim),
+    digest: JSON.parse(block.digest),
+    data: JSON.parse(block.data)
+  };
+}
+
 class BlockController {
   async getBlocks(ctx) {
     const { page, pageSize } = extractPage(ctx);
@@ -18,20 +34,7 @@ class BlockController {
       raw: true
     });
 
-    const fieldsNeedTrim = [
-      "hash",
-      "parent_hash",
-      "state_root",
-      "extrinsics_root"
-    ];
-    const items = blocks.map(block => {
-      return {
-        ...block,
-        ...trimFields(block, fieldsNeedTrim),
-        digest: JSON.parse(block.digest),
-        data: JSON.parse(block.data)
-      };
-    });
+    const items = blocks.map(normalizeBlock);
 
     ctx.body = {
       items,
@@ -39,6 +42,30 @@ class BlockController {
       page,
       pageMax: Math.floor(count / pageSize)
     };
+  }
+
+  async getBlock(ctx) {
+    const { heightOrHash } = ctx.params;
+
+    const option = { raw: true };
+    if (/^\d+$/.test(heightOrHash)) {
+      Object.assign(option, { where: { number: heightOrHash } });
+    } else if (/^0x[\da-fA-F]{64}$/.test(heightOrHash)) {
+      Object.assign(option, { where: { hash: heightOrHash.toLowerCase() } });
+    } else {
+      ctx.status = 400;
+      ctx.body = { error: "invalid param" };
+      return;
+    }
+
+    const block = await ctx.db.Block.findOne(option);
+    if (!block) {
+      ctx.status = 404;
+      ctx.body = { error: "not found" };
+      return;
+    }
+
+    ctx.body = normalizeBlock(block);
   }
 }
 
