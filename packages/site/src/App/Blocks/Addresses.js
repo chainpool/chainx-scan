a3d87b215b0a166cf5c1c072748b1b60304b7cfc
weixiaoyi <2278095567@qq.com>
2019-03-04 10:18:04 +0800

    区块列表分页请列表

--- packages/site/src/App/Blocks/Addresses.js
+++ packages/site/src/App/Blocks/Addresses.js
@@ -1,53 +1,14 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { fetchAndSetPageBlocks } from "@store/action";
+import PageBlocksTable from "./PageBlocksTable";
 class Addresses extends PureComponent {
-  componentDidMount() {
-    this.props.fetchAndSetPageBlocks(1, 20).then(res => {
-      console.log(this.props.state);
-      console.log(res, "============");
-    });
-  }
   render() {
-    const { pageBlocks: { blocks = [] } = {} } = this.props;
     return (
       <div>
-        <table className="table is-striped is-narrow is-hoverable is-fullwidth">
-          <thead>
-            <tr>
-              <th>账户地址</th>
-              <th>PCX 总余额</th>
-              <th>BTC 总余额</th>
-            </tr>
-          </thead>
-          <tbody>
-            {blocks.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>1</td>
-                  <td>2</td>
-                  <td>3</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <PageBlocksTable />
       </div>
     );
   }
 }
-
-const mapStateToProps = state => ({
-  state,
-  pageBlocks: state.pageBlocks
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchAndSetPageBlocks: (page, pageSize) =>
-    dispatch(fetchAndSetPageBlocks(page, pageSize))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Addresses);
+export default Addresses;
