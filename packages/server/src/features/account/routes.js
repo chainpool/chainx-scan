28f5c607c662f9bfa750ff8e3b2e021ffd50b0f4
Yongfeng LI <wliyongfeng@gmail.com>
2019-03-13 17:25:14 +0800

    Add api to get account nominations

--- packages/server/src/features/account/routes.js
+++ packages/server/src/features/account/routes.js
@@ -1,8 +1,10 @@
 const Router = require("koa-router");
 const accountController = require("./account.controller");
+const nominationController = require("./nomination.controller");
 
 const router = new Router();
 router.get("/intentions", accountController.intentions);
 router.get("/accounts", accountController.accounts);
+router.get("/account/:accountId/nominations", nominationController.accountNominations);
 
 module.exports = router;
