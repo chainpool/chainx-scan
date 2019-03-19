28eed454f4b778d59c88716cec153c55ee60ba4d
yznt <nsso@foxmail.com>
2019-03-19 18:57:06 +0800

    hash

--- packages/site/src/pages/CrossTxs.js
+++ packages/site/src/pages/CrossTxs.js
@@ -64,7 +64,7 @@ export function RenderCrossTxs({ tableProps, tableData, handleChange }) {
         },
         {
           title: "块哈希",
-          dataIndex: "version"
+          dataIndex: "hash"
         },
         {
           title: "交易时间",
