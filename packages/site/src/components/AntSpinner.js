ccedd041e2aa8946b59d8bb055fa625e13070668
lgyytm <592676835@qq.com>
2019-04-08 17:22:36 +0800

    change spinner to singleself

--- packages/site/src/components/AntSpinner.js
+++ packages/site/src/components/AntSpinner.js
@@ -1,18 +1,10 @@
 import React from "react";
+import Spin from "antd/lib/spin";
 
 export default function Spinner() {
   return (
-    <div className="ant-spin-nested-loading spinner-wrap">
-      <div>
-        <div className="ant-spin ant-spin-spinning ant-table-without-pagination ant-table-spin-holder">
-          <span className="ant-spin-dot ant-spin-dot-spin">
-            <i />
-            <i />
-            <i />
-            <i />
-          </span>
-        </div>
-      </div>
+    <div className="spinner-wrap">
+      <Spin />
     </div>
   );
 }
