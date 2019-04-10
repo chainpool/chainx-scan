7edda1a6cf1ba7002c08bda648fc9df8f2d69bbc
lgyytm <592676835@qq.com>
2019-04-10 18:51:11 +0800

    correct address link content

--- packages/site/src/components/AddressLink.js
+++ packages/site/src/components/AddressLink.js
@@ -3,19 +3,16 @@ import { NavLink } from "react-router-dom";
 import classnames from "classnames";
 import { hexAddPrefix } from "@polkadot/util";
 
-import { useAppContext } from "./AppContext";
+import { encodeAddress } from "../shared";
 
 export default memo(function AddressLink(props) {
   const { value, className, style } = props;
   const hexValue = hexAddPrefix(value);
 
-  const [{ intentions = [] }] = useAppContext();
-
   let showValue = "";
 
   if (hexValue !== "0x") {
-    const { name = "" } = intentions.find(({ accountid }) => accountid === hexValue) || {};
-    showValue = name;
+    showValue = encodeAddress(hexValue);
   }
 
   return (
