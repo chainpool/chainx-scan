412e38954bcf57f537bdef03ec00a562ab896f5a
yznt <nsso@foxmail.com>
2019-04-11 16:39:56 +0800

    change number component

--- packages/site/src/components/Number.js
+++ packages/site/src/components/Number.js
@@ -1,17 +1,13 @@
 import { memo } from "react";
 
-export default memo(function Number(props) {
-  const { value, useGrouping = true, precision = 0 } = props;
+export default memo(function ToNumber(props) {
+  const { value, options } = props;
   if (isNaN(value)) return "";
-  const options = {
-    useGrouping
-  };
 
-  const no = value / Math.pow(10, precision);
+  const no = Number(value);
 
-  if (precision) {
-    options.minimumFractionDigits = precision;
-  }
-
-  return no.toLocaleString(undefined, options);
+  return no.toLocaleString(undefined, {
+    useGrouping: true,
+    ...options
+  });
 });
