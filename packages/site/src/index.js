7a9b3019107a8fa3ee6d6b67371be43d04ba7cb7
lgyytm <592676835@qq.com>
2019-04-22 11:58:25 +0800

    init i18n

--- packages/site/src/index.js
+++ packages/site/src/index.js
@@ -4,5 +4,20 @@ import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import "./index.scss";
 import App from "./pages/App";
+//引入locale配置文件，具体路径根据实际情况填写
+import zh_CN from "./i18n/zh_CN";
+import en_US from "./i18n/en-US";
+//如果浏览器没有自带intl，则需要在使用npm安装intl之后添加如下代码
+import { IntlProvider } from "react-intl";
+const local = navigator.language;
+const i18n = {
+  zh_CN,
+  en_US
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <IntlProvider locale={local} messages={i18n[local]}>
+    <App />
+  </IntlProvider>,
+  document.getElementById("root")
+);
