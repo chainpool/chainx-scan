923bc08b10c79c877c591316aa4561a9197aa735
Yongfeng LI <wliyongfeng@gmail.com>
2019-04-25 11:00:37 +0800

    Correct missed blocks api url

--- packages/server/src/features/account/routes.js
+++ packages/server/src/features/account/routes.js
@@ -9,12 +9,12 @@ router.get("/psedu_intentions", intentionController.pseduIntentions);
 router.get("/intention/:accountId", intentionController.intention);
 router.get("/intention/:accountId/nominations", nominationController.byNominee);
 router.get("/intention/:accountId/settings", intentionController.setting);
+router.get("/intention/:accountId/missed_blocks", intentionController.missedBlocks);
 router.get("/accounts", accountController.accounts);
 router.get("/account/:accountId/nominations", nominationController.accountNominations);
 router.get("/account/:accountId/detail", accountController.accountDetail);
 router.get("/account/:accountId/txs", accountController.txs);
 router.get("/account/:accountId/binds", accountController.bindAddresses);
 router.get("/account/:accountId/fill_orders", accountController.fillOrders);
-router.get("/account/:accountId/missed_blocks", intentionController.missedBlocks);
 
 module.exports = router;
