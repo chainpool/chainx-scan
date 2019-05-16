50d550507faafe0c1ed9e5cdb6201a8784ba120d
yznt <nsso@foxmail.com>
2019-05-16 11:41:02 +0800

    upgrade deps

--- packages/site/config/webpack.config.js
+++ packages/site/config/webpack.config.js
@@ -23,7 +23,6 @@ const getClientEnvironment = require("./env");
 const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin-alt");
 const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
-
 const common = require("./webpack.config.common");
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
@@ -43,7 +42,7 @@ const sassModuleRegex = /\.module\.(scss|sass)$/;
 
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
-module.exports = function(webpackEnv, report) {
+module.exports = function(webpackEnv) {
   const isEnvDevelopment = webpackEnv === "development";
   const isEnvProduction = webpackEnv === "production";
 
@@ -59,7 +58,6 @@ module.exports = function(webpackEnv, report) {
   // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
   // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
   const publicUrl = isEnvProduction ? publicPath.slice(0, -1) : isEnvDevelopment && "";
-  const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
   // Get environment variables to inject into our app.
   const env = getClientEnvironment(publicUrl);
 
@@ -108,7 +106,7 @@ module.exports = function(webpackEnv, report) {
     return loaders;
   };
 
-  const webpackConfig = {
+  return {
     mode: isEnvProduction ? "production" : isEnvDevelopment && "development",
     // Stop compilation early in production
     bail: isEnvProduction,
@@ -152,10 +150,6 @@ module.exports = function(webpackEnv, report) {
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info => path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, "/")
         : isEnvDevelopment && (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, "/"))
-    },
-    externals: {
-      react: "React",
-      "react-dom": "ReactDOM"
     },
     optimization: {
       minimize: isEnvProduction,
@@ -272,7 +266,6 @@ module.exports = function(webpackEnv, report) {
         PnpWebpackPlugin.moduleLoader(module)
       ]
     },
-    target: "web",
     module: {
       strictExportPresence: true,
       rules: [
@@ -474,7 +467,6 @@ module.exports = function(webpackEnv, report) {
             : undefined
         )
       ),
-      new webpack.optimize.ModuleConcatenationPlugin(),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       isEnvProduction && shouldInlineRuntimeChunk && new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime~.+[.]js/]),
@@ -569,8 +561,7 @@ module.exports = function(webpackEnv, report) {
           watch: paths.appSrc,
           silent: true,
           formatter: typescriptFormatter
-        }),
-      report && new BundleAnalyzerPlugin({ analyzerPort: 4000 })
+        })
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
@@ -587,5 +578,4 @@ module.exports = function(webpackEnv, report) {
     // our own hints via the FileSizeReporter
     performance: false
   };
-  return webpackConfig;
 };
