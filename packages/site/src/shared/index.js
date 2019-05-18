c836ae1f37be309932d0cc33c4aa31b778bdfd34
lgyytm <592676835@qq.com>
2019-05-19 00:18:53 +0800

    fixed event pagination

--- packages/site/src/shared/index.js
+++ packages/site/src/shared/index.js
@@ -2,3 +2,4 @@ export { default as encodeAddress } from "./encodeAddress";
 export { default as decodeAddress } from "./decodeAddress";
 export { default as useRedux } from "./useRedux";
 export { default as createStore } from "./createStore";
+export { default as shallowEqual } from "./shallowEqual";
