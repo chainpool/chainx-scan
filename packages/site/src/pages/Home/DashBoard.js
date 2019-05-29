f5d862b013bad209ee20f578791fc77da281a593
Yongfeng LI <wliyongfeng@gmail.com>
2019-05-29 18:30:49 +0800

    Fix typo

--- packages/site/src/pages/Home/DashBoard.js
+++ packages/site/src/pages/Home/DashBoard.js
@@ -29,7 +29,7 @@ export default function DashBoard() {
     {
       label: (
         <>
-          Chainx
+          ChainX
           <FormattedMessage id="TRANSACTIONCOUNT" />
         </>
       ),
