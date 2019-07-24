946af19470152ac6317ef04ec420e995a59bd675
yznt <nsso@foxmail.com>
2019-07-24 17:20:01 +0800

    update

--- packages/site/src/pages/Referendum/index.js
+++ packages/site/src/pages/Referendum/index.js
@@ -49,12 +49,9 @@ export default function Referendum() {
                             </p>
                           )}
                           <p>
-                            <span className="r-title">公投规则：</span>{" "}
-                            {item.rule && (
-                              <span>
-                                1、用户投票效力以投票账户的PCX总余额计数；2、公投过程中用户可随时更改投票意见，以最后一次投票为有效票；3、截至投票结束时，赞成票数大于2/3即提案通过，反之则提案未通过。
-                              </span>
-                            )}
+                            <span className="r-title">公投规则：</span>
+                            {item.rule ||
+                              "1、用户投票效力以投票账户的PCX总余额计数；2、公投过程中用户可随时更改投票意见，以最后一次投票为有效票；3、截至投票结束时，赞成票数大于2/3即提案通过，反之则提案未通过。"}
                             <span className="r-title">参与方式：</span>用户向以下指定地址
                             <span className="red">转账 0 PCX</span>表达对本提案持赞成或反对意见。
                             <span className="r-title">公投结束时间：</span>块高
@@ -98,8 +95,9 @@ export default function Referendum() {
                             </p>
                           )}
                           <p>
-                            <span className="r-title">公投规则：</span>{" "}
-                            1、用户投票效力以投票账户的PCX总余额计数；2、公投过程中用户可随时更改投票意见，以最后一次投票为有效票；3、截至投票结束时，赞成票数大于反对票数即为提案通过，反之则提案未通过。
+                            <span className="r-title">公投规则：</span>
+                            {item.rule ||
+                              "1、用户投票效力以投票账户的PCX总余额计数；2、公投过程中用户可随时更改投票意见，以最后一次投票为有效票；3、截至投票结束时，赞成票数大于2/3即提案通过，反之则提案未通过。"}
                             <span className="r-title">参与方式：</span>用户向以下指定地址
                             <span className="red">转账 0 PCX</span>表达对本提案持赞成或反对意见。
                             <span className="r-title">公投结束时间：</span>块高
