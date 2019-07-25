aefa468d50cf1ca3de0bcc2d16a9dd13e5c622bf
yznt <nsso@foxmail.com>
2019-07-25 21:41:13 +0800

    calcTotal

---
+++ packages/server/src/features/referendum/calcTotal.js
@@ -0,0 +1,98 @@
+const fetch = require("node-fetch");
+const { Observable, timer, merge } = require("rxjs");
+const { timeout, retryWhen, delayWhen, reduce, take } = require("rxjs/operators");
+
+const concurrent = 100; // 并发数量
+
+const log = console;
+
+const rpcUrl = "https://w1.chainx.org.cn/rpc";
+const assetsToken = ["PCX", "BTC", "SDOT"];
+
+function timeoutAndRetry(time = 10000, retryTime = 10000, msg) {
+  return source$ =>
+    source$.pipe(
+      timeout(time),
+      retryWhen(errors => {
+        if (msg) log.error(msg);
+        return errors.pipe(delayWhen(val => timer(retryTime)));
+      }),
+      take(1)
+    );
+}
+
+function queryAccountAssets(hash, accountid) {
+  return new Observable(async subscriber => {
+    try {
+      const requestData = {
+        id: `${accountid}`,
+        jsonrpc: "2.0",
+        method: "chainx_getAssetsByAccount",
+        params: [`${accountid}`, 0, 100, `${hash}`]
+      };
+
+      const res = await fetch(rpcUrl, {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestData)
+      });
+      const {
+        result: { data }
+      } = await res.json();
+
+      const asstesData = data
+        .map(({ details, name }) => {
+          return [
+            name,
+            details.Free +
+              details.ReservedCurrency +
+              details.ReservedDexFuture +
+              details.ReservedDexSpot +
+              details.ReservedStaking +
+              details.ReservedStakingRevocation +
+              details.ReservedWithdrawal
+          ];
+        })
+        .reduce((result, [name, value]) => {
+          if (isNaN(value)) {
+            const msg = `${accountid} 资产总额相加错误`;
+            log.error(msg);
+            throw new Error(msg);
+          }
+          if (!assetsToken.includes(name)) {
+            const msg = `${accountid} 资产总额相加错误`;
+            log.error(msg);
+            throw new Error(msg);
+          }
+          result[name] = value;
+          return result;
+        }, {});
+      // log.info(`${accountid} 资产：${JSON.stringify(asstesData)}`);
+      subscriber.next([accountid, asstesData]);
+      subscriber.complete();
+    } catch (error) {
+      if (!subscriber.closed) {
+        log.error(`${accountid} 查询错误`);
+      }
+      subscriber.error(error);
+    }
+  }).pipe(timeoutAndRetry(10000, 10000, `${accountid} 查询超时重试`));
+}
+
+module.exports = function calcTotal(hash, allAccounts) {
+  log.info("开始执行任务");
+  return merge(
+    ...allAccounts.map(accountId => {
+      return queryAccountAssets(hash, accountId);
+    }),
+    concurrent
+  ).pipe(
+    reduce((acc, [accountid, data]) => {
+      acc[accountid] = data["PCX"];
+      return acc;
+    }, {})
+  );
+};
