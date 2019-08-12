749f5a89622ca01a2f4c2a4a31c67aae0a6fefde
yznt <nsso@foxmail.com>
2019-08-12 17:45:35 +0800

    fix

--- packages/server/src/features/referendum/referendum.controller.js
+++ packages/server/src/features/referendum/referendum.controller.js
@@ -184,7 +184,7 @@ async function getDetails() {
   return config.map(item => {
     return {
       ...item,
-      status: currentBlock - +item.deadBlock > 0 ? true : false,
+      isFinished: currentBlock - +item.deadBlock > 0 ? true : false,
       deadTime: currentTime.add((+item.deadBlock - currentBlock) * 2, "s").format("YYYY-MM-DD HH:mm:ss")
     };
   });
