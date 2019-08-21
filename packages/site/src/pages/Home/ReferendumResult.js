e0e30774641dc11a8ef1c7fd7bbc0f4065709829
yznt <nsso@foxmail.com>
2019-08-21 15:23:32 +0800

    update

---
+++ packages/site/src/pages/Home/ReferendumResult.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+import { TxLink, AddressLink, TxAction, AntSpinner as Spinner } from "../../components";
+import { ReactComponent as Right } from "../../assets/right.svg";
+import api from "../../services/api";
+import { useRedux } from "../../shared";
+import { FormattedMessage } from "react-intl";
+
+export default function BestTransactions() {
+  const [{ txs }, setState] = useRedux("bestTransactions", { txs: [] });
+
+  useEffect(() => {
+    const subscription = api.fetchLatestTxs$().subscribe(data => setState({ txs: data }));
+    return () => subscription.unsubscribe();
+  }, [api]);
+
+  const loading = (
+    <tr style={{ height: 370, background: "#fff" }}>
+      <td colSpan="3" style={{ verticalAlign: "middle" }}>
+        <Spinner />
+      </td>
+    </tr>
+  );
+
+  return (
+    <section className="panel">
+      <div className="panel-heading">
+        <FormattedMessage id="NEWESTTRANSACTION" />
+      </div>
+      <div className="panel-block">
+        <table className="table is-striped is-fullwidth data-table">
+          <thead>
+            <tr>
+              <th>
+                <FormattedMessage id="TRANSACTIONHASH" />
+              </th>
+              <th>
+                <FormattedMessage id="SENDER" />
+              </th>
+              <th className="has-text-right">
+                <FormattedMessage id="ACTION" />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {txs && txs.length
+              ? txs.map(tx => (
+                  <tr key={tx.hash}>
+                    <td>
+                      <TxLink style={{ width: 136 }} className="text-truncate" value={tx.hash} />
+                    </td>
+                    <td>
+                      <AddressLink style={{ width: 180 }} className="text-truncate" value={tx.signed} />
+                    </td>
+                    <td className="has-text-right">
+                      <TxAction module={tx.module} call={tx.call} />
+                    </td>
+                  </tr>
+                ))
+              : loading}
+          </tbody>
+        </table>
+      </div>
+      <div className="panel-block panel-footer-link" style={{ justifyContent: "center" }}>
+        <NavLink className="view-more" to="/txs">
+          <FormattedMessage id="SHOWALL" />
+          <Right className="right" />
+        </NavLink>
+      </div>
+    </section>
+  );
+}
--- packages/site/src/pages/Home/index.js
+++ packages/site/src/pages/Home/index.js
@@ -4,6 +4,7 @@ import DashBoard from "./DashBoard";
 import BtcStatus from "./BtcStatus";
 import BestBlocks from "./BestBlocks";
 import BestTransactions from "./BestTransactions";
+import ReferendumResult from "./ReferendumResult";
 
 export default function Home() {
   return (
@@ -16,6 +17,9 @@ export default function Home() {
         <div className="column">
           <BestTransactions />
         </div>
+        {/* <div className="column">
+          <ReferendumResult />
+        </div> */}
       </div>
       <BtcStatus />
     </section>
