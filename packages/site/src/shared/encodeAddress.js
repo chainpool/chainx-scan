7cb7b06dc555d33d40ef8bd20132589e4359b876
yznt <nsso@foxmail.com>
2019-11-15 16:50:55 +0800

    update

--- packages/site/src/shared/encodeAddress.js
+++ packages/site/src/shared/encodeAddress.js
@@ -2,7 +2,7 @@ import encodeAddress from "@polkadot/keyring/address/encode";
 import setAddressPrefix from "@polkadot/keyring/address/setPrefix";
 import hexAddPrefix from "@polkadot/util/hex/addPrefix";
 
-setAddressPrefix(process.env.REACT_APP_SERVER);
+setAddressPrefix(process.env.REACT_APP_SERVER || 44);
 
 export default function _encodeAddress(publicKey) {
   if (!publicKey) return publicKey;
