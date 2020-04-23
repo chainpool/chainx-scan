af10c72f28852ee55393d4c880521335bb5c652e
Yongfeng LI <wliyongfeng@gmail.com>
2020-04-23 12:03:48 +0800

    Remove console.log

--- packages/site/src/services/utils.js
+++ packages/site/src/services/utils.js
@@ -1,6 +1,4 @@
 export const getDefaultApi = () => {
-  console.log(process.env.USE_DEV);
-  console.log(process.env.REACT_APP_SERVER);
   if (process.env.USE_DEV === "1") {
     return process.env.REACT_APP_SERVER;
   }
