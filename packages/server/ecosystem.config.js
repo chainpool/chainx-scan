0bda05eb678856027d9094bb7d2925d7072d3f17
Yongfeng LI <wliyongfeng@gmail.com>
2020-05-21 10:07:00 +0800

    Add time format for log

--- packages/server/ecosystem.config.js
+++ packages/server/ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "ChainX scan server",
       script: "src/index.js",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
       env: {
         NODE_ENV: "development"
       },
